4c9db6f247e8f31de499dd129b5706b06d3c130c
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-08-11 14:25:55 +0530

    Update timing.py

--- magtap/timing.py
+++ magtap/timing.py
@@ -33,6 +33,6 @@ def call_gettoas(bestprof, pfd, timfile):
     logging.basicConfig(level=logging.INFO, format='%(message)s')
     logging.info(f"[INTERNAL: GetTOAs]: bestprof = {bestprof}; pfd = {pfd}; timfile = {timfile} ")
     logging.info("----------")
-    subprocess.call(["get_TOAs.py","-t", bestprof, "-n", str(4), pfd, ">", timfile])
+    subprocess.call(["get_TOAs.py","-t", bestprof, "-n", str(4), pfd])
     
     
